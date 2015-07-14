e837e27d9c7252e9d1520da864b2867cbfe9b815
Adam Lockwood <alockwood@clearvision-cm.com>
2015-07-14 13:37:07 +0100

    may use these libraries

--- src/main.lua
+++ src/main.lua
@@ -13,6 +13,7 @@ local t
 
 -- Load some default values for our rectangle.
 function love.load()
+
     udp = socket.udp()
     udp:settimeout(0)
     udp:setpeername(address, port)
@@ -22,18 +23,23 @@ function love.load()
     t = 0 -- (re)set t to 0
 
     x, y, w, h = 20, 20, 60, 20;
+
 end
 
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
+
   if w < 200 then
     w = w + 1;
     h = h + 1;
   end
+
 end
 
 -- Draw a coloured rectangle.
 function love.draw()
+
     love.graphics.setColor(0, 100, 100);
     love.graphics.rectangle('fill', x, y, w, h);
+
 end
