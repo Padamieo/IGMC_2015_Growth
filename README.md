dd8b17a1f6b82bb8fd7fc22815c358c060849606
Adam Lockwood <alockwood@clearvision-cm.com>
2015-07-15 14:10:59 +0100

    added basic player need to refine further

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ http://contest.gamedevfort.com/
 current tutorials might help
 
 -- https://love2d.org/forums/viewtopic.php?f=5&t=3550
+
 -- http://www.osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-1-of-3
+
 -- https://love2d.org/wiki/Tutorial:Networking_with_UDP
+
+-- https://love2d.org/wiki/HUMP
--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,59 @@
 debug = true
 
-local socket = require("socket")
-
--- the address and port of the server
-local address, port = "localhost", 12345
-
-local entity -- entity is what we'll be controlling
-local updaterate = 0.1 -- how long to wait, in seconds, before requesting an update
-
 local world = {} -- the empty world-state
-local t
+
+camera = {}
+camera.x = 0
+camera.y = 0
+camera.scaleX = 1
+camera.scaleY = 1
+camera.rotation = 0
+
+function camera:set()
+  love.graphics.push()
+  love.graphics.rotate(-self.rotation)
+  love.graphics.scale(1 / self.scaleX, 1 / self.scaleY)
+  love.graphics.translate(-self.x, -self.y)
+end
+
+function camera:unset()
+  love.graphics.pop()
+end
+
+function camera:move(dx, dy)
+  self.x = self.x + (dx or 0)
+  self.y = self.y + (dy or 0)
+end
+
+function camera:rotate(dr)
+  self.rotation = self.rotation + dr
+end
+
+function camera:scale(sx, sy)
+  sx = sx or 1
+  self.scaleX = self.scaleX * sx
+  self.scaleY = self.scaleY * (sy or sx)
+end
+
+function camera:setPosition(x, y)
+  self.x = x or self.x
+  self.y = y or self.y
+end
+
+function camera:setScale(sx, sy)
+  self.scaleX = sx or self.scaleX
+  self.scaleY = sy or self.scaleY
+end
+
+function camera:mousePosition()
+  return love.mouse.getX() * self.scaleX + self.x, love.mouse.getY() * self.scaleY + self.y
+end
 
 -- Load some default values for our rectangle.
 function love.load()
+
+    player = { x = 200, y = 200, speed = 150, img = nil }
+    player.img = love.graphics.newImage('img/player.png')
 
     udp = socket.udp()
     udp:settimeout(0)
@@ -20,26 +61,51 @@ function love.load()
     local dg = string.format("%s %s %d %d", entity, 'at', 320, 240)
     udp:send(dg) -- the magic line in question.
     -- t is just a variable we use to help us with the update rate in love.update.
-    t = 0 -- (re)set t to 0
 
     x, y, w, h = 20, 20, 60, 20;
+g = 1;
+
 
 end
 
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
 
-  if w < 200 then
-    w = w + 1;
-    h = h + 1;
+  if love.keyboard.isDown('left','a') then
+  	if player.x > 0 then -- binds us to the map
+  		player.x = player.x - (player.speed*dt)
+  	end
+  elseif love.keyboard.isDown('right','d') then
+  	if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+  		player.x = player.x + (player.speed*dt)
+  	end
   end
 
+  if love.keyboard.isDown('up','aw') then
+  	if player.y > 0 then -- binds us to the map
+  		player.y = player.y - (player.speed*dt)
+  	end
+  elseif love.keyboard.isDown('down','s') then
+  	if player.y < (love.graphics.getHeight() - player.img:getHeight()) then
+  		player.y = player.y + (player.speed*dt)
+  	end
+  end
+
+    camera:scale(g) -- zoom by 3
+    --spacex = love.graphics.getHeight() - player.img:getHeight()
+    camera:setPosition(player.x, player.y)
+    --g = g + 0.001;
 end
 
 -- Draw a coloured rectangle.
 function love.draw()
-
-    love.graphics.setColor(0, 100, 100);
+    camera:set()
+    love.graphics.setColor(0, 88, 100);
     love.graphics.rectangle('fill', x, y, w, h);
+    love.graphics.rectangle('fill', 80, 80, w, h);
+    love.graphics.rectangle('fill', 250, 250, w, h);
+    love.graphics.setColor(255, 255, 255);
+    love.graphics.draw(player.img, player.x, player.y)
+    camera:unset()
 
 end
