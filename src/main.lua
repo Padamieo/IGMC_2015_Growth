8fa9a0a4e9070feac9a89fc97b787b23beefb18c
Adam Lockwood <alockwood@clearvision-cm.com>
2015-08-11 14:27:54 +0100

    refining xbox360 pad setup

--- src/main.lua
+++ src/main.lua
@@ -9,6 +9,7 @@ lick.reset = true
 
 --define game states and functions to be included
 menu = require "menu"
+team = require "menu_side"
 --team = require "team"
 --game = require "game"
 
@@ -26,95 +27,6 @@ function love.load()
     --gamestate.switch(menu)
     gamestate.switch(team)
 end
-
-team = {}
-
-function love.joystickadded(joystick)
-
-  --p1joystick = joystick
-
-  if gamepad  ~= nil then
-    local value = table.getn(gamepad)
-    print(value)
-    gamepad[value+1] = joystick
-  else
-    gamepad = {}
-    gamepad[1] = joystick
-  end
-
-end
-
-function team:enter()
-  love.graphics.setBackgroundColor( 0, 10, 25 )
-
-  gui = {}
-  gui.keyboard = {x = (love.graphics.getWidth()/2)-25, y = 25}
-  gui.keyboard.shape = love.physics.newRectangleShape(0, 0, 50, 50)
-
-  roster = {}
-
-  gui.pad = {}
-
-  if gamepad ~= nil then
-    for i,v in ipairs(gamepad) do
-      gui.pad[i] = {x = (love.graphics.getWidth()/2)-25, y = 150}
-      gui.pad[i].shape = love.physics.newRectangleShape(0, 0, 50, 25)
-    end
-  end
-
-end
-
-function team:update()
-
-  if love.keyboard.isDown('left','a') then
-    gui.keyboard.x = (love.graphics.getWidth()/4)-25
-    roster[1] = {team = 0, c = "K"}
-  elseif love.keyboard.isDown('right','d') then
-    gui.keyboard.x = (love.graphics.getWidth()/2)+(love.graphics.getWidth()/4)-25
-    roster[1] = {team = 1, c = "K"}
-  end
-
-  if gamepad ~= nil then
-    for i,v in ipairs(gamepad) do
-      local x = gamepad[i]:getGamepadAxis("leftx")
-      if x < -0.2 then
-        gui.pad[i].x = (love.graphics.getWidth()/4)-25
-        roster[i+1] = {team = 0, c = i}
-      elseif x > 0.2 then
-        gui.pad[i].x = (love.graphics.getWidth()/2)+(love.graphics.getWidth()/4)-25
-        roster[i+1] = {team = 1, c = i}
-      end
-    end
-  end
-
-end
-
-function team:draw()
-  love.graphics.setColor(250, 250, 250);
-  love.graphics.print("Press g to continue", 10, 10)
-  love.graphics.setColor(250, 50, 50);
-
-  love.graphics.rectangle( "fill", gui.keyboard.x, gui.keyboard.y, 50, 50 )
-
-  if gamepad ~= nil then
-    for i,v in ipairs(gamepad) do
-      love.graphics.rectangle( "fill", gui.pad[i].x, gui.pad[i].y, 50, 50 )
-    end
-  end
-
-end
-
-function team:keyreleased(key, code)
-
-  if key == 'g' then
-      print(table.getn(roster)) --list number of players, 10 need in total computer assigned.
-
-      gamestate.switch(game)
-  end
-end
-
-
-
 
 --following to go in game.lua but bellow for development
 game = {}
@@ -324,11 +236,14 @@ function game:enter()
   objects.goal_B.fixture:setUserData("B")
 
   --this is roster of listed players, will be built elsewhere
-  roster = {}
-    roster[1] = {x = 300, y = 0, c = "K", team = 0, char = "default"}
-    roster[2] = {x = -300, y = -0, c = 1, team = 1, char = "default"}
-    roster[3] = {x = -500, y = -450, c = "C", team = 1, char = "default"}
-    roster[4] = {x = -500, y = 450, c = "C", team = 1, char = "default"}
+
+  if table.getn(roster) == 0 then
+    roster = {}
+      roster[1] = {x = 300, y = 0, c = "K", team = 0, char = "default"}
+      roster[2] = {x = -300, y = -0, c = "C", team = 1, char = "default"}
+      roster[3] = {x = -500, y = -450, c = "C", team = 1, char = "default"}
+      roster[4] = {x = -500, y = 450, c = "C", team = 1, char = "default"}
+  end
 
   player = {}
   ani = {}
---
+++ src/menu_side.lua
@@ -0,0 +1,87 @@
+local team = {}
+
+--will this work here?
+function love.joystickadded(joystick)
+  --p1joystick = joystick
+  if gamepad  ~= nil then
+    local value = table.getn(gamepad)
+    print(value)
+    gamepad[value+1] = joystick
+  else
+    gamepad = {}
+    gamepad[1] = joystick
+  end
+
+end
+
+function team:enter()
+  love.graphics.setBackgroundColor( 0, 10, 25 )
+
+  gui = {}
+  gui.keyboard = {x = (love.graphics.getWidth()/2)-25, y = 25}
+  gui.keyboard.shape = love.physics.newRectangleShape(0, 0, 50, 25)
+
+  roster = {}
+
+  gui.pad = {}
+
+  if gamepad ~= nil then
+    for i,v in ipairs(gamepad) do
+      --just print out 10 make them invisible in draw
+      gui.pad[i] = {x = (love.graphics.getWidth()/2)-25, y = 75}
+      gui.pad[i].shape = love.physics.newRectangleShape(0, 0, 50, 25)
+    end
+  end
+
+end
+
+function team:update()
+
+  if love.keyboard.isDown('left','a') then
+    gui.keyboard.x = (love.graphics.getWidth()/4)-25
+    roster[1] = {x = 0, y = 0, c = "K", team = 0, char = "default"}
+  elseif love.keyboard.isDown('right','d') then
+    gui.keyboard.x = (love.graphics.getWidth()/2)+(love.graphics.getWidth()/4)-25
+    roster[1] = {x = 0, y = 0, c = "K", team = 1, char = "default"}
+  end
+
+  if gamepad ~= nil then
+    for i,v in ipairs(gamepad) do
+      local x = gamepad[i]:getGamepadAxis("leftx")
+      if x < -0.2 then
+        gui.pad[i].x = (love.graphics.getWidth()/4)-25
+        roster[i+1] = {x = 0, y = 0, c = i, team = 0, char = "default"}
+      elseif x > 0.2 then
+        gui.pad[i].x = (love.graphics.getWidth()/2)+(love.graphics.getWidth()/4)-25
+        roster[i+1] = {x = 0, y = 0, c = i, team = 1, char = "default"}
+      end
+    end
+  end
+
+end
+
+function team:draw()
+  love.graphics.setColor(250, 250, 250);
+  love.graphics.print("Press g to continue", 10, 10)
+  love.graphics.setColor(250, 50, 50);
+
+  love.graphics.rectangle( "fill", gui.keyboard.x, gui.keyboard.y, 50, 50 )
+
+  if gamepad ~= nil then
+    for i,v in ipairs(gamepad) do
+      love.graphics.rectangle( "fill", gui.pad[i].x, gui.pad[i].y, 50, 50 )
+    end
+  end
+
+end
+
+function team:keyreleased(key, code)
+
+  if key == 'g' then
+      print(table.getn(roster)) --list number of players, 10 need in total computer assigned.
+
+      gamestate.switch(game)
+  end
+end
+
+return team;
